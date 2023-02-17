b77928d583d60a234282d9d426d555356f7971ac
collowserem <collinskipkemei224@gmail.com>
2023-02-17 05:55:21 -0800

    preprocessor

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-/*main - A program that prints a line  with puts function
- * Return: 0 (successs)
+/**
+ * main - A program that print a line with puts function
+ * Return: 0 (sucess)
  */
 int main(void)
 {
